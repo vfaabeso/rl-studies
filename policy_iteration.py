fd64db0c9a63300c1068f9f049dc07c4a8347e4a
vfaabeso <farrelbeso@gmail.com>
2024-04-04 23:25:27 +0800

    the example works now

--- policy_iteration.py
+++ policy_iteration.py
@@ -1,15 +1,15 @@
 import numpy as np
 
 # hyperparameters
-theta = 1e-4
-discount = 0.95
+theta = 1e-6
+discount = 0.90
 
 # environment functions
 state_width = 4
 state_height = 4
 state_size = state_width * state_height
 action_size = 4
-terminal = (0, 15)
+terminal = (0,15)
 
 # necessary variables
 state_value_table = np.random.randn(state_size)
@@ -57,11 +57,56 @@ def get_transition_prob(state_from: int, state_to: int) -> float:
     if state_to in adjacent_states: return 1/len(adjacent_states)
     else: return 0
 
-# # policy evaluation
-# def policy_evaluation() -> None:
-#     delta = 0
-#     while True:
-#         for state in range(state_size):
-#             value = state_value_table[state]
-#             # evaluate the summation
-#             bellman_value = 0
+def get_next_state(state: int, action: int) -> int:
+    x, y = _state_to_coords(state)
+    if action == 0: y -= 1
+    elif action == 1: x += 1
+    elif action == 2: y += 1
+    elif action == 3: x -= 1
+    # if invalid, then stay
+    new_state = _coord_to_state(x, y)
+    if not _is_valid_state(new_state): return state
+    else: return new_state
+
+# policy evaluation
+def policy_evaluation() -> None:
+    delta = 0
+    while True:
+        for state in range(state_size):
+            value = state_value_table[state]
+            # since there's just one possible next state
+            policy_action = get_policy(state)
+            next_state = get_next_state(state, policy_action)
+            bellman = get_reward(next_state) + discount*state_value_table[next_state]
+            state_value_table[next_state] = bellman
+            delta = abs(value - state_value_table[next_state])
+            if delta < theta: return
+
+# policy improvement
+def policy_improvement() -> bool:
+    policy_stable = True
+    for state in range(state_size):
+        old_action = get_policy(state)
+        # define the bellman equation here
+        action_values = {}
+        for action in range(action_size):
+            # since there's one possible next state
+            next_state = get_next_state(state, action)
+            expected_reward = get_reward(next_state) + discount*state_value_table[next_state]
+            action_values[action] = expected_reward
+        # then get which is the maximum
+        argmax = max(action_values, key=action_values.get)
+        policy_table[state] = argmax
+        if old_action != argmax: policy_stable = False
+    return policy_stable
+
+# loop
+policy_stable = False
+iteration = 0
+while not policy_stable:
+    print(f'Iteration #{iteration}')
+    policy_evaluation()
+    policy_stable = policy_improvement()
+    iteration += 1
+print(policy_table)
+print(state_value_table)
